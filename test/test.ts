12bfe0a8b936283e5dd4cfc999dc0afaa19bca01
heroboy <ywqywq@yeah.net>
2020-01-17 10:25:50 +0800

    add some test code comment

--- test/test.ts
+++ test/test.ts
@@ -7,9 +7,15 @@ import { Project } from 'ts-morph'
 
 function TestWithFile(fileName: string)
 {
+	//测试方法：
+	//在测试的ts文件中，用/*a*/ 表示一个检测点。（一个字符，不能有空格）
+	//在任意一行 //a=>[XX,YY] ，表示在这个插入点对应生成的case语句的内容。
+	//然后下面的测试代码，就会解析ts文件中的上面两种内容，然后测试，看看生成的代码是不是符合。
 	describe("Test file:" + fileName, function ()
 	{
 		let project = new Project({ useInMemoryFileSystem: true });
+		//这里直接修改了ts-morph的js代码，使得能获得host
+		//几个null，没有用到就算了。我估计只有在language server中才会有这几个对象吧，所以也没办法获取到。
 		let info: ts.server.PluginCreateInfo = {
 			project: null,
 			languageService: project.getLanguageService().compilerObject,
@@ -51,9 +57,9 @@ function TestWithFile(fileName: string)
 					assert.equal(edits.edits.length, 1, 'edits length must be 1');
 					assert.equal(edits.edits[0].textChanges.length, 1, 'textChanges length must be 1');
 					let actualGenCases = util.extractAllCaseItems(edits.edits[0].textChanges[0].newText);
-					if (!util.arrayEqual(actualGenCases,result.items))
+					if (!util.arrayEqual(actualGenCases, result.items))
 					{
-						assert(0,'\nwant result:' + result.items.join(',') + '\nactual result: ' + actualGenCases.join(','));
+						assert(0, '\nwant result:' + result.items.join(',') + '\nactual result: ' + actualGenCases.join(','));
 					}
 				}
 			});
